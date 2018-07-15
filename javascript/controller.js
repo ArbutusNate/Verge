02005664a23104612a04a909ad33420def28f940
ArbutusNate <arbutusnate@gmail.com>
2018-07-15 13:24:35 -0700

    ground-up redesign initial commit

--- javascript/controller.js
+++ javascript/controller.js
@@ -6,29 +6,9 @@ var playerLocation = '0,0,0';
 
 // Functions
 
-const getZone = (playerLocation) => {
-  // Find the location in World.JSON.
-  let current = World[playerLocation];
-  let $textToAdd
-  // Set up DOM element
-  if(current.visited === false) {
-    // First Visit
-    $textToAdd = $newText('story', current.description);
-  } else {
-    // Revisits
-    $textToAdd = $newText('story', current.revisit);
-  };
-  //Append <p> to text box.
-  $('#text-box').append($textToAdd);
-}
-
-const $newText = (htmlClass, content) => {
-  // Builds <p> DOM element.
-  return $(`<p class="${htmlClass}"> ${content} </p>`)
-}
-
 const movePlayer = (clicked) => {
   // Turn string coordinates to array.
+  // debugger;
   let locationArray = playerLocation.split(",").map(Number);
   console.log(locationArray);
   //Get data from DOM buttons.
@@ -44,12 +24,56 @@ const movePlayer = (clicked) => {
   //Save Visits in local storage? cookies?
 }
 
+const getZone = (playerLocation) => {
+  // Find the location in World.JSON.
+  let current = World[playerLocation];
+  let $textToAdd
+  // Set up DOM element
+  !checkVisit(playerLocation) ? 
+    $textToAdd = $NewText('story', current.description)
+    :
+    $textToAdd = $NewText('story', current.revisit)
+  //Append <p> to text box.
+  $('#text-box').append($textToAdd);
+  saveChangesLocal(playerLocation);
+}
+
+const $NewText = (htmlClass, content) => {
+  // Builds <p> DOM element.
+  return $(`<p class="${htmlClass}"> ${content} </p>`)
+}
+
+const checkVisit = (playerLocation) => {
+  let alreadyVisited = localStorage.getItem('visited');
+  console.log(`Already Visited: ${alreadyVisited}`);
+  if (alreadyVisited.includes(playerLocation)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const saveChangesLocal = (visited, effects) => {
+  // if already visited, do nothing
+  if (checkVisit(visited)){
+    return
+  } else {
+  // if unvisited save location as visited
+    let alreadyVisited = localStorage.getItem('visited');
+    let nowVisited = alreadyVisited + ';' + visited;
+    localStorage.setItem('visited', nowVisited);
+    console.log(localStorage.getItem('visited'))
+  }
+}
+
 // Startup/Controller
 $().ready( () => {
   // AJAX call for World.JSON
   worldImport = $.getJSON('javascript/world.json', () => {
   // Get rid of AJAX metadata.
   World = worldImport.responseJSON.world
+  localStorage.setItem('visited', '');
+  localStorage.setItem('effects', '');
   // Fire it up
   getZone(playerLocation);
   });
